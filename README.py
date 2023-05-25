8c753e2110fedb87abe399e50f548318b0745cfd
s25284410 <64548029+s25284410@users.noreply.github.com>
2023-05-25 17:17:13 +0800

    python focus_timer.py

--- README.py
+++ README.py
@@ -1,13 +1,13 @@
 import time
 
-minrtes = int(input("Enter the number of minutes to focus: "))
+minutes = int(input("請輸入要專注的分鐘數: "))
 seconds = minutes * 60
 
 while seconds:
     mins, secs = divmod(seconds, 60)
-    timer = '{:20d}:{:02d}'.format(mins, secs)
+    timer = '{:02d}:{:02d}'.format(mins, secs)  # 修正此行，將原本的 20d 改為 02d
     print(timer, end="\r")
     time.sleep(1)
-    secinds -= 1
-    
-print("Time's up")
+    seconds -= 1  # 修正此行，將原本的 secinds 改為 seconds
+
+print("時間到！")
