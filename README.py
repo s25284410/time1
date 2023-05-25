eec485294a85c0aabc7726c6690afc664a581f6f
s25284410 <64548029+s25284410@users.noreply.github.com>
2023-05-25 17:02:16 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,19 +1,13 @@
-# time1
-import datetime
 import time
 
-# 定義專注時間的起始和結束時間
-start_time = datetime.time(9, 0, 0)   # 9點
-end_time = datetime.time(17, 0, 0)    # 17點
+minrtes = int(input("Enter the number of minutes to focus: "))
+seconds = minutes * 60
 
-while True:
-    current_time = datetime.datetime.now().time()
-
-    # 檢查是否在專注時間範圍內
-    if start_time <= current_time <= end_time:
-        print("現在是專注時間！")
-    else:
-        print("現在不是專注時間。")
-
-    # 暫停一秒
+while seconds:
+    mins, secs = divmod(seconds, 60)
+    timer = '{:20d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
     time.sleep(1)
+    secinds -= 1
+    
+print("Time's up")
